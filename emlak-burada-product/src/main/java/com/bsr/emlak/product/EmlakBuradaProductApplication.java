0cb6e579c9341f43d79a902935803f2ba0f5b282
Büşra Erçelik <bsr.ercelik@gmail.com>
2022-03-20 12:51:25 +0300

    added product microservice

---
+++ emlak-burada-product/src/main/java/com/bsr/emlak/product/EmlakBuradaProductApplication.java
@@ -0,0 +1,13 @@
+package com.bsr.emlak.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmlakBuradaProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmlakBuradaProductApplication.class, args);
+    }
+
+}
